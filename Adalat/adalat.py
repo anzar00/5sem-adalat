9be5d9b3554aeeb8b664de6c9c2f04f957d4ceca
Anzar <peerzada.anzar@gmail.com>
2021-12-28 04:42:32 +0530

    Basic Mockup

--- Adalat/adalat.py
+++ Adalat/adalat.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
