fb96c20c204884e8ee5f00fdadc5d4d4fa9daafc
Anzar <peerzada.anzar@gmail.com>
2021-12-28 06:36:19 +0530

    Login and Register

--- Adalat/adalat.py
+++ Adalat/adalat.py
@@ -1,13 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app = Flask(__name__)
+app.secret_key = '23092000'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'DBAdmin@123$'
+app.config['MYSQL_DB'] = 'adalat'
+
+mysql = MySQL(app)
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     return render_template('home.html')
+@app.route("/home", methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM MasterLogin WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('home.html', msg = msg)
 
+
+@app.route('/logout')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
 
 @app.route("/about")
 def about_page():
     return render_template('about.html')
+
+@app.route("/register")
+def register_page():
+    return render_template('register.html')
