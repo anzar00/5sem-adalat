a72f5696b3eb5e705bf8b45dc8bd39e7a98f53e2
Anzar <peerzada.anzar@gmail.com>
2021-12-30 04:23:58 +0530

    Role Based Registration with Mails

--- Adalat/adalat.py
+++ Adalat/adalat.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import re
+import re, string, random
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = Flask(__name__)
 app.secret_key = '23092000'
@@ -13,12 +16,15 @@ app.config['MYSQL_DB'] = 'adalat'
 
 mysql = MySQL(app)
 
+#Home Page
 @app.route("/")
 @app.route("/home")
 def home_page():
     if 'loggedin' in session:
-        return render_template('./admin/index.html', username=session['username'])
+        return render_template('./admin/index.html', email=session['email'])
     return render_template('home.html')
+
+#Admin Login
 @app.route("/home", methods=['GET', 'POST'])
 def login():
     msg = ''
@@ -45,7 +51,67 @@ def login():
     # Show the login form with message (if any)
     return render_template('home.html', msg = msg)
 
+#Citizen Login
+@app.route("/login/citizen")
+def citizen_login_page():
+    if 'loggedin' in session:
+        return render_template('./citizen/index.html', username=session['username'])
+    return render_template('home.html')
+@app.route("/login/citizen", methods=['GET', 'POST'])
+def citizen_login():
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        email = request.form['email']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Citizen WHERE email = %s AND password = %s', (email, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['ClientId']
+            session['email'] = account['email']
+            session['name'] = account['Name']
+            # Redirect to home page
+            return render_template('./citizen/index.html', email=session['email'])
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('home.html', msg = msg)
 
+
+@app.route("/login/lawyer", methods=['GET', 'POST'])
+def lawyer_login():
+    msg = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        email = request.form['email']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Citizen WHERE email = %s AND password = %s', (email, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['email'] = account['email']
+            # Redirect to home page
+            return render_template('./lawyer/index.html', email=session['email'])
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect email/password!'
+    # Show the login form with message (if any)
+    return render_template('home.html', msg = msg)
+
+#Logout API
 @app.route('/logout')
 def logout():
    session.pop('loggedin', None)
@@ -54,10 +120,12 @@ def logout():
    # Redirect to login page
    return redirect(url_for('login'))
 
+#About Page
 @app.route("/about")
 def about_page():
     return render_template('about.html')
 
+#Admin Registration
 @app.route("/register",)
 def register_page():
     return render_template('register.html')
@@ -90,37 +158,107 @@ def register():
         msg = 'Please fill out the form!'
     return render_template('register.html', msg=msg)
 
+#Citizen Registration
 @app.route("/register/citizen")
 def register_citizen_page():
     return render_template('register-citizen.html')
 @app.route("/register/citizen", methods=['GET', 'POST'])
 def register_citizen():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == 'POST' and 'name' in request.form and 'email' in request.form and 'contact' in request.form and 'address' in request.form:
+        name = request.form['name']
         email = request.form['email']
+        contact = request.form['contact']
+        address = request.form['address']
+
+        #Random Password Generation
+        characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+        def generate_random_password():
+
+            length = 8
+            random.shuffle(characters)
+            password = []
+                
+            for i in range(length):
+                password.append(random.choice(characters))
+                
+            random.shuffle(password)
+
+            return ("".join(password))
+
+        #Password Generation End
+
+        pass_word = generate_random_password()
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM MasterLogin WHERE username = %s', (username,))
+        cursor.execute('SELECT * FROM Citizen WHERE email = %s', (email,))
         account = cursor.fetchone()
 
         if account:
             msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
+        elif not name or not contact or not address:
             msg = 'Please fill out the form!'
         else:
-            cursor.execute('INSERT INTO MasterLogin VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            
+            cursor.execute('INSERT INTO Citizen (Name,email,contact,address,password) VALUES (%s, %s, %s, %s, %s)', (name, email, contact, address, pass_word,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
+
+            # Sending Mail
+            
+
+            sender_email = "adalat.app@gmail.com"
+            receiver_email = email
+            password = 'Adalat@123$'
+
+            message = MIMEMultipart("alternative")
+            message["Subject"] = ("Dear "+ name +" , Please Note Your Credentials For Adalat")
+            message["From"] = sender_email
+            message["To"] = receiver_email
+
+            # Create the plain-text and HTML version of your message
+            text = """\
+            Hi, """+name+"""
+            Please note your credentials for Adalat<br>
+            Your Email is - """+email+""", and
+            Password is - ""+pass_word+
+            """
+            html = """\
+            <html>
+            <body>
+                <p>Hi,"""+name+"""<br>
+                Please note your credentials for Adalat<br>
+                Your <b>Email</b> is - """+email+""", and<br>
+                <b>Password</b> is - """+pass_word+"""
+                </p>
+            </body>
+            </html>
+            """
+
+            # Turn these into plain/html MIMEText objects
+            part1 = MIMEText(text, "plain")
+            part2 = MIMEText(html, "html")
+
+            # Add HTML/plain-text parts to MIMEMultipart message
+            # The email client will try to render the last part first
+            message.attach(part1)
+            message.attach(part2)
+
+            # Create secure connection with server and send email
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                server.login(sender_email, password)
+                server.sendmail(
+                    sender_email, receiver_email, message.as_string()
+                )
+            # End Mail 
     
     elif request.method == 'POST':
         msg = 'Please fill out the form!'
-    return render_template('register.html', msg=msg)
+    return render_template('register-citizen.html', msg=msg)
 
 
     
